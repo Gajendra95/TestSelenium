e0184c5a426e4366541a7d1bbf035ea50ae4bd9c
Gajendra <gajendra.ranjith@gmail.com>
2019-09-10 12:08:32 +0530

    Add project files.

---
+++ MoodleTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MoodleTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MoodleTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("176b56d5-af28-4dcc-8060-b19cc0b6458d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MoodleTest/TestSelenium.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+namespace MoodleTest
+{
+    [TestClass]
+    public class TestSelenium
+    {
+        static IWebDriver driver;
+
+
+        [AssemblyInitialize]
+        public static void SetUp(TestContext context)
+        {
+            driver = new ChromeDriver();
+
+        }
+
+        [TestMethod]
+        public void TestChromeDriver()
+        {
+            driver.Navigate().GoToUrl("http://172.16.51.98/moodle/login");
+
+            // Get the page elements
+            var userNameField = driver.FindElement(By.Id("username"));
+            var userPasswordField = driver.FindElement(By.Id("password"));
+            //var loginButton = driver.FindElement(By.Id("//input[@value='Login']");
+
+            // Type user name and password
+            userNameField.SendKeys("admin");
+            userPasswordField.SendKeys("Moodle@123");
+
+            // and click the login button
+            //loginButton.Click();
+
+
+
+            //driverGC.Navigate().GoToUrl("http://www.google.com/");
+            //driverGC.FindElement(By.Id("lst-ib")).SendKeys("Tapas Pal Codeguru");
+            //driverGC.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
+        }
+    }
+}
